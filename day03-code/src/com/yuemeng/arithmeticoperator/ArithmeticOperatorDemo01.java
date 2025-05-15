b1d26ab46932e28e08a8eb58c4945ef7ec51001c
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-16 00:37:10 +0800

    运算符-算数运算符-笔记

---
+++ day03-code/src/com/yuemeng/arithmeticoperator/ArithmeticOperatorDemo01.java
@@ -0,0 +1,19 @@
+package com.yuemeng.arithmeticoperator;
+
+public class ArithmeticOperatorDemo01 {
+    public static void main(String[] args) {
+        // +
+        System.out.println(5 + 3);
+        // -
+        System.out.println(5 - 3);
+
+        //*
+        System.out.println(5 * 3);
+        // /
+        System.out.println(5 / 3);
+        System.out.println(5.0 / 3);
+
+        // %
+        System.out.println(5 % 3);
+    }
+}
