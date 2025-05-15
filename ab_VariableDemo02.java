a3993393a67c2c76421dd4da8544b970487e41a0
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-15 22:48:44 +0800

    基本数据类型-笔记

---
+++ ab_VariableDemo02.java
@@ -0,0 +1,32 @@
+public class ab_VariableDemo02 {
+    // 主入口
+    public static void main(String[] args) {
+        // byte
+        byte a = 10;
+        System.out.println(a);
+        // short
+        short b = 20;
+        System.out.println(b);
+        // int
+        int c = 30;
+        System.out.println(c);
+        // long
+        long d = 9999999999L;
+        System.out.println(d);
+
+        // float
+        float e = 3.14F;
+        System.out.println(e);
+        // double
+        double f = 3.14;
+        System.out.println(f);
+
+        // char
+        char g = '月';
+        System.out.println(g);
+
+        // boolean
+        boolean h = true;
+        System.out.println(h);
+    }
+}
