8e9ebaef52bd68f9633abaf0c2c4fbed69fe7172
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-15 23:53:04 +0800

    输入-练习

---
+++ ac_ScannerText01.java
@@ -0,0 +1,17 @@
+// 导包
+import java.util.Scanner;
+
+public class ac_ScannerText01 {
+    public static void main(String[] args) {
+        // 创建对象
+        Scanner sc = new Scanner(System.in);
+        // 接收数据
+        System.out.println("请输入第一个整数：");
+        int i = sc.nextInt();
+
+        System.out.println("请输入第二个整数：");
+        int j = sc.nextInt();
+
+        System.out.println("i + j = " + (i + j));
+    }
+}
