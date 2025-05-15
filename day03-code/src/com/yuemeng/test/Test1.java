7b51821f5c8173c8e49402f6353b5c17abca8061
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-16 01:37:19 +0800

    运算符-算数运算符-练习

---
+++ day03-code/src/com/yuemeng/test/Test1.java
@@ -0,0 +1,22 @@
+package com.yuemeng.test;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // 键盘录入一个三位数，获取其中的个位，十位，百位
+
+        // 1.键盘录入
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个三位数：");
+        int number = sc.nextInt();
+
+        int ge = number % 10;
+        int shi = number / 10 % 10;
+        int bai = number / 100;
+
+        System.out.println("个位：" + ge);
+        System.out.println("十位：" + shi);
+        System.out.println("百位：" + bai);
+    }
+}
