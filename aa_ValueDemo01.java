750f1fe4600949bd015a46bbe836df65ac541977
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-15 20:41:00 +0800

    正常提交

---
+++ aa_ValueDemo01.java
@@ -0,0 +1,23 @@
+public class aa_ValueDemo01 {
+    public static void main(String[] args) {
+        // 整数
+        System.out.println(10);
+        System.out.println(-10);
+
+        // 浮点数
+        System.out.println(10.0);
+        System.out.println(-3.14);
+
+        // 字符串
+        System.out.println("Hello World");
+        System.out.println("中文");
+
+        // 字符
+        System.out.println('a');
+        System.out.println('男');
+
+        // 布尔
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
---
+++ ab_VariableDemo01.java
@@ -0,0 +1,11 @@
+public class ab_VariableDemo01 {
+    // 主入口
+    public static void main(String[] args) {
+        // 定义变量
+        int a;
+        // 赋值
+        a = 10;
+        // 输出
+        System.out.println(a);
+    }
+}
