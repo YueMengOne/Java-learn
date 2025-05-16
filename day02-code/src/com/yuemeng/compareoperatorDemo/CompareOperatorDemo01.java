ac9f2f2b0f9c98b27e0532016abd9826e7b15c4f
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-16 17:52:45 +0800

    Day02笔记-运算符-关系运算符

---
+++ day02-code/src/com/yuemeng/compareoperatorDemo/CompareOperatorDemo01.java
@@ -0,0 +1,17 @@
+package com.yuemeng.compareoperatorDemo;
+
+public class CompareOperatorDemo01 {
+    public static void main(String[] args) {
+        // == 判断两边的值是否相等
+        int a = 10;
+        int b = 20;
+        int c = 10;
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+
+        System.out.println("-------------------------");
+
+        // != 判断两边的值是否不相等
+    }
+}
---
+++ day02-code/src/com/yuemeng/compareoperatorDemo/CompareOperatorText01.java
@@ -0,0 +1,28 @@
+package com.yuemeng.compareoperatorDemo;
+
+import java.util.Scanner;
+
+public class CompareOperatorText01 {
+    public static void main(String[] args) {
+        /*
+        * 您和您的约会对象在餐厅里面正在约会。
+        * 键盘录入两个整数，表示你和你约会对象衣服的时髦度。
+        * （手动录入0~10之间的整数，不能录其他）
+        * 如果你的时髦程度大于你对象的时髦程度，相亲就成功，输出true。
+        * 否则输出false。
+        */
+
+        // 1. 键盘录入两个整数，表示你和你约会对象衣服的时髦度。
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的时髦程度：");
+        int my = scanner.nextInt();
+        System.out.println("请输入你的对象时髦程度：");
+        int you = scanner.nextInt();
+
+        // 2. 比较两个整数的大小
+        boolean result = my > you;
+
+        // 3. 输出结果
+        System.out.println(result);
+    }
+}
