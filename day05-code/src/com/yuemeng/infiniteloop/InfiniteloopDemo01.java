b9360813a950c3b6b7aaf7d7fa91934f62ada896
YueMeng-MacPro <woyuguohui@outlook.com>
2025-05-20 03:16:28 +0800

    Day05 笔记

---
+++ day05-code/src/com/yuemeng/infiniteloop/InfiniteloopDemo01.java
@@ -0,0 +1,15 @@
+package com.yuemeng.infiniteloop;
+
+public class InfiniteloopDemo01 {
+    public static void main(String[] args) {
+        // for 循环的死循环
+//        for (;;){
+//            System.out.println("死循环");
+//        }
+
+        // while 循环的死循环
+        while (true){
+            System.out.println("死循环");
+        }
+    }
+}
---
+++ day05-code/src/com/yuemeng/skiploop/SkipLoopDemo01.java
@@ -0,0 +1,4 @@
+package com.yuemeng.skiploop;
+
+public class SkipLoopDemo01 {
+}
